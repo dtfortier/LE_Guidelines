cb58ecef278468613050b0b48b358a812b8d8174
Danielle Fortier <dfortier@localedge.com>
2015-02-24 16:59:46 -0500

    New changes to pages

--- sections/colors.php
+++ sections/colors.php
@@ -2,15 +2,33 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                <h1>Colors</h1>
-        <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#477DB3</span></span></div>
-        <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated">#f79720</span></span></div>
-        <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated">#f9f9f9</span></span></div>
-        <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated">#edeced</span></span></div>
-        <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated">#cbcccb</span></span></div>
-        <div class="sg-color sg-color--f"><span class="sg-color-swatch"><span class="sg-animated">#4c5965</span></span></div>
-        <div class="sg-color sg-color--g"><span class="sg-color-swatch"><span class="sg-animated">#364149</span></span></div>
-
+                    <h1>Colors</h1>
+                    <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#477DB3</span></span></div>
+                    <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated">#f79720</span></span></div>
+                    <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated">#f9f9f9</span></span></div>
+                    <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated">#edeced</span></span></div>
+                    <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated">#cbcccb</span></span></div>
+                    <div class="sg-color sg-color--f"><span class="sg-color-swatch"><span class="sg-animated">#4c5965</span></span></div>
+                    <div class="sg-color sg-color--g"><span class="sg-color-swatch"><span class="sg-animated">#364149</span></span></div>
+                </div>
+                <div class="clearfix"></div>
+                
+                <div class="content-wrap">
+                    <div class="content-icon"><i class="fa fa-question-circle"></i></div>
+                    
+                    <h2><i class="fa fa-question-circle"></i> Where Are These Colors Used?</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Suspendisse urna leo, dapibus a sapien laoreet, tincidunt maximus eros. 
+                        Quisque ullamcorper molestie est sed rutrum. Suspendisse vel ultrices ante, 
+                        eget volutpat quam. Sed et semper velit, vel sollicitudin felis. Cras sit amet 
+                        enim commodo, ullamcorper sem et, fermentum velit. Mauris id porta purus. Donec 
+                        mauris enim, auctor nec felis eu, malesuada lobortis metus. Sed vel sapien luctus, 
+                        laoreet nibh ac, vestibulum augue. Quisque at vulputate dui, eu varius erat. Phasellus 
+                        hendrerit ex et dui porta, ac tempor diam congue. Fusce ipsum nibh, consequat nec dui nec, 
+                        condimentum pellentesque tellus. Sed ut tincidunt tortor. Quisque vel euismod velit, 
+                        tempor placerat dolor.
+                    </p>
                 </div>
             </div>
         </div>
--- sections/intro.php
+++ sections/intro.php
@@ -3,8 +3,21 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Localedge Design Standards Basics</h1>
-                    <p></p>
-                    <p>This is the introductory page.</p>
+                    <div class="content-wrap">
+                        <h2>This is the Introductory Page</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Suspendisse urna leo, dapibus a sapien laoreet, tincidunt maximus eros. 
+                            Quisque ullamcorper molestie est sed rutrum. Suspendisse vel ultrices ante, 
+                            eget volutpat quam. Sed et semper velit, vel sollicitudin felis. Cras sit amet 
+                            enim commodo, ullamcorper sem et, fermentum velit. Mauris id porta purus. Donec 
+                            mauris enim, auctor nec felis eu, malesuada lobortis metus. Sed vel sapien luctus, 
+                            laoreet nibh ac, vestibulum augue. Quisque at vulputate dui, eu varius erat. Phasellus 
+                            hendrerit ex et dui porta, ac tempor diam congue. Fusce ipsum nibh, consequat nec dui nec, 
+                            condimentum pellentesque tellus. Sed ut tincidunt tortor. Quisque vel euismod velit, 
+                            tempor placerat dolor.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
